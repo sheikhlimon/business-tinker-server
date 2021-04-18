6654a8f03c037d145bf9763218d2ab08d6786c96
sheikhlimon <sheikhlimon404@gmail.com>
2021-04-18 19:16:41 +0600

    update

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 * added post method for both services and reviews
 * added get method for both services and reviews
+* post method for admin and finding admin
 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x0gki.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -21,6 +22,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const businessCollection = client.db("businessTinker").collection("services");
   const reviewCollection = client.db("businessTinker").collection("reviews");
+  const adminCollection = client.db("businessTinker").collection("admins");
   app.post('/addService', (req, res) => {
     const addService = req.body;
     businessCollection.insertOne(addService)
@@ -50,6 +52,30 @@ client.connect(err => {
       res.send(documents)
     })
   })
+
+  app.get("/book/:id", (req, res) => {
+    businessCollection.find({_id: ObjectId(req.params.id)})
+    .toArray ( (err, documents) => {
+      res.send(documents[0]);
+    })
+  })
+
+  app.post('/isAdmin', (req, res) => {
+        const email = req.body.email;
+        adminCollection.find({ email: email })
+            .toArray((err, admins) => {
+                res.send(admins.length > 0);
+            })
+    })
+
+    app.post('/addAdmin', (req, res) => {
+      const addAdmin = req.body;
+      adminCollection.insertOne(addAdmin)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+    })
+
 });
 
 app.listen(process.env.PORT || port)
